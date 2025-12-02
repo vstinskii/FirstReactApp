dca698fc161fda7a0d49bfc392d45b47ae500644
Vitalii Stinskii <vstinskij@gmail.com>
2025-12-02 19:41:27 +0200

    added SafeAreaView, styles practice

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.headerText}>Hello World!!</Text>
+        <Text style={{
+          fontSize: 20,
+          textAlign: "justify"
+        }} >Lorem Ipsum is simply dummy text of the
+          <Text
+            style={{ 
+              color: "red",
+              textDecorationLine: "underline"
+             }}
+
+            onPress={() => Alert.alert("Text has been pressed")}
+          > printing </Text>
+          and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+          PageMaker including versions of Lorem Ipsum.</Text>
+        {/* <StatusBar style="auto" /> */}
+      </SafeAreaView>
+    </SafeAreaProvider>
+
   );
 }
 
@@ -14,7 +37,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 20, // <--- This sets the font size
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 10,
   },
 });
