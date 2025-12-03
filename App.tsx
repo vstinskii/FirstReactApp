f2a7da841bf2970cfc4ff88c3b49c219dbfd53e8
Vitalii Stinskii <vstinskij@gmail.com>
2025-12-03 16:01:50 +0200

    refactor styles

--- App.tsx
+++ App.tsx
@@ -8,24 +8,18 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <Text style={styles.headerText}>Hello World!!</Text>
-        <Text style={{
-          fontSize: 20,
-          textAlign: "justify"
-        }} >Lorem Ipsum is simply dummy text of the
+        <Text style={styles.mainText} >Lorem Ipsum is simply dummy text of the
           <Text
-            style={{ 
-              color: "red",
-              textDecorationLine: "underline"
-             }}
-
+            style={styles.clickText}
             onPress={() => Alert.alert("Text has been pressed")}
           > printing </Text>
           and typesetting industry.
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          It has survived not only five centuries, but also the leap into electronic typesetting,
           remaining essentially unchanged. It was popularised in the 1960s with the release of
-          Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem Ipsum.</Text>
+          Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+          like Aldus PageMaker including versions of Lorem Ipsum.</Text>
         {/* <StatusBar style="auto" /> */}
       </SafeAreaView>
     </SafeAreaProvider>
@@ -46,4 +40,12 @@ const styles = StyleSheet.create({
     color: '#333',
     marginBottom: 10,
   },
+  mainText: {
+    fontSize: 20,
+    textAlign: "justify"
+  },
+  clickText: {
+    color: "red",
+    textDecorationLine: "underline"
+  }
 });
