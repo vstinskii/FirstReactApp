a4690f868b6b2c8a2cce4fe23ff495d270380ef9
Vitalii Stinskii <vstinskij@gmail.com>
2025-12-09 17:15:13 +0200

    added view prop, cleaned code format

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Platform, Alert, Image, Button, TouchableOpacity, Pressable, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Platform, Alert, Image, Button, TouchableOpacity, Pressable, ScrollView, ActivityIndicator } from 'react-native';
 
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
@@ -11,46 +11,49 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <ScrollView
+          contentContainerStyle={styles.main_scroll}
 
-        contentContainerStyle={styles.main_scroll}
-
-        horizontal={false}
+          horizontal={false}
         >
-        <Text style={styles.headerText}>Hello World!!</Text>
-        <Text style={styles.headerText}>This is {Platform.OS === "android" ? "Android" : "IOS"} device</Text>
-        
-        <Text style={styles.mainText} >Lorem Ipsum is simply dummy text of the
-          <Text
-            style={styles.clickText}
-            onPress={() => Alert.alert("Text has been pressed")}
-          > printing </Text>
-          and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-          It has survived not only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with the release of
-          Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-          like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+          <Text style={styles.headerText}>Hello World!!</Text>
+          <Text style={styles.headerText}>This is {Platform.OS === "android" ? "Android" : "IOS"} device</Text>
+
+          <Text style={styles.mainText} >Lorem Ipsum is simply dummy text of the
+            <Text
+              style={styles.clickText}
+              onPress={() => Alert.alert("Text has been pressed")}
+            > printing </Text>
+            and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+            like Aldus PageMaker including versions of Lorem Ipsum.</Text>
 
           <TouchableOpacity>
-          <Image 
-          source={require("./assets/icon.png")}
-          style={styles.localImg}
-          />
+            <Image
+              source={require("./assets/icon.png")}
+              style={styles.localImg}
+            />
           </TouchableOpacity>
-          
+
           <Pressable onPress={onButtonPress}>
-          <Image 
-          source={{uri: "https://vitalii-stinskii.com/images/photo.png"}}
-          style={styles.uriImg}
-          />
+            <Image
+              source={{ uri: "https://vitalii-stinskii.com/images/photo.png" }}
+              style={styles.uriImg}
+            />
           </Pressable>
-          
-          <Button 
-          title="Press"
-          onPress={onButtonPress}
+
+          <Button
+            title="Press"
+            onPress={onButtonPress}
           />
-        {/* <StatusBar style="auto" /> */}
+          {/* <StatusBar style="auto" /> */}
+          <View style={styles.view1}>
+            <Text>Some view text</Text>
+          </View>
+          <ActivityIndicator size={"large"} color={"green"} />
         </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -92,5 +95,12 @@ const styles = StyleSheet.create({
     marginRight: 20,
     marginLeft: 20,
     marginTop: 12
+  },
+  view1: {
+    height: 300,
+    width: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(251, 243, 156, 1)"
   }
 });
