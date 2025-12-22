c18cf6fe42892adc3888bf6e04f4796d9ccc5410
Vitalii Stinskii <vstinskij@gmail.com>
2025-12-22 18:17:42 +0200

    added modal

---
+++ src/screens/ReactModal.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, Button, View, Modal } from 'react-native'
+import React, { useState } from 'react'
+
+
+const ReactModal = () => {
+
+    const [modalVisible, setModalVisible] = useState(false)
+
+    const showModal = () => setModalVisible(true)
+    const hideModal = () => setModalVisible(false)
+    
+    return (
+        <View style={styles.container}>
+        <Button title="Show modal"
+        onPress={showModal}
+        />
+
+        <Modal visible={modalVisible} animationType='slide'>
+            <Text style={{marginTop: 50, fontSize: 25}}>Here is the modal</Text>
+            <Button 
+            title="Hide modal" 
+            
+            onPress={hideModal}
+        />
+        </Modal>
+        </View>
+    )
+}
+
+export default ReactModal
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+        justifyContent: "center",
+        backgroundColor: "tomato",
+        alignItems: "center"
+
+    },
+    
+})
+ 
